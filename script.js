419ece4c86133fe2effa62e84ce619c00abb81a5
uvacoder <uva22940coder@gmail.com>
2024-08-29 12:38:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const filterInput = document.querySelector('.filter-repos');
 // get information from github profile
 const getProfile = async () => {
     const res = await fetch(
-        `https://api.github.com/users/${username}`
+        `https://api.github.com/users/${uvacoder}`
         // {
         //     headers: {
         //         Accept: 'application/vnd.github+json',
@@ -50,7 +50,7 @@ const getRepos = async () => {
     let res;
     for (let i = 1; i <= maxPages; i++) {
         res = await fetch(
-            `https://api.github.com/users/${username}/repos?&sort=pushed&per_page=100&page=${i}`
+            `https://api.github.com/users/${uvacoder}/repos?&sort=pushed&per_page=100&page=${i}`
             // {
             //     headers: {
             //         Accept: 'application/vnd.github+json',
@@ -70,7 +70,7 @@ getRepos();
 
 // display list of all user's public repos
 const displayRepos = (repos) => {
-    const userHome = `https://github.com/${username}`;
+    const userHome = `https://github.com/${uvacoder}`;
     filterInput.classList.remove('hide');
     for (const repo of repos) {
         if (repo.fork && hideForks) {
